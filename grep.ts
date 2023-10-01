a4e81a5e778252816be62b7a0ee9ce1b9f168c2b
mashimashi0 <masajob48@gmail.com>
2023-10-01 15:40:19 +0900

    first commit

---
+++ grep.ts
@@ -0,0 +1,34 @@
+import * as io from "https://deno.land/std@0.165.0/io/mod.ts";
+import { walk } from "https://deno.land/std@0.165.0/fs/walk.ts";
+
+export type GrepResult = {
+    fileName: string;
+    line: number;
+    text: string;
+};
+
+export async function grep(
+    root: string,
+    word: string,
+): Promise<GrepResult[]> {
+    const result = [];
+    for await (const entry of walk(root, { skip: [/^.git/] })) {
+        if (entry.isFile){
+            const file = await Deno.open(entry.path);
+            try {
+                let line = 1;
+                for await (const text of io.readLines(file)) {
+                    result.push({
+                        fileName: entry.path,
+                        line: line,
+                        text: text,
+                    });
+                }
+                line++;
+            } finally {
+                file.close();
+            }
+        }
+    }
+    return result;
+}
